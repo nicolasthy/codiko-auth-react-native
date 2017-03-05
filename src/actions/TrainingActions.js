d3bed603074ef0d5f8797680f768d51a4be62e8b
Louis Larpin <louis.larpin@gmail.com>
2017-03-05 07:37:42 +0100

    refacto training actions WIP

--- src/actions/TrainingActions.js
+++ src/actions/TrainingActions.js
@@ -7,38 +7,44 @@ import {
   FETCH_THEMATICAL_TRAININGS_SUCCESS
 } from './types';
 
-const getTrainingsByType = (token, serie, getState) => {
-  const { host: apiHost, path: apiPath, headers } = getState().api.endpoint;
-  let endpoint = `${apiHost}/api/trainings?filter[serie_serie_type_eq]=${serie.type}&filter[completed_at_not_null]=1&sort=-completed_at`;
-
-  if(serie.category) {
-    endpoint += `&filter[serie_category_eq]=${serie.category}`;
-  }
-
-  return apiRequest(endpoint, {
-    headers,
-    method: 'GET',
-  });
-}
-
 export const fetchTrainingsByType = (token, serie) => {
   return (dispatch, getState) => {
     return new Promise((resolve, reject) => {
-      getTrainingsByType(token, serie, getState)
-        .then(({ data }) => {
-          if(serie.type === 0) {
-            dispatch({ type: FETCH_PEDAGOGICAL_TRAININGS_SUCCESS, payload: data });
-          }
-          else if(serie.type === 1) {
-            dispatch({ type: FETCH_THEMATICAL_TRAININGS_SUCCESS, payload: data, category: serie.category });
-          }
-          else {
-            dispatch({ type: FETCH_EXAMINATION_TRAININGS_SUCCESS, payload: data });
-          }
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      const filters = Object.assign({
+        serie_serie_type_eq: serie.type,
+        completed_at_not_null: 1
+      },
+        serie.category.length && { serie_category_eq: serie.category }
+      )
+
+      const filterQuery = Object.keys(filters).map(key => {
+        return `filter[${key}]=${filters[key]}`
+      }).join('&');
+
+      const { host: apiHost, path: apiPath, headers } = getState().api.endpoint;
+      const endpoint = `${apiHost}${apiPath}/trainings?${filterQuery}&sort=-completed_at`
+
+      apiRequest(endpoint, {
+        headers,
+        method: 'GET'
+      })
+      .then(data => {
+        // TODO
+        // Do we really need this ? If yes, use const and swicth/case !
+        if(serie.type === 0) {
+          dispatch({ type: FETCH_PEDAGOGICAL_TRAININGS_SUCCESS, payload: data });
+        }
+        else if(serie.type === 1) {
+          dispatch({ type: FETCH_THEMATICAL_TRAININGS_SUCCESS, payload: data, category: serie.category });
+        }
+        else {
+          dispatch({ type: FETCH_EXAMINATION_TRAININGS_SUCCESS, payload: data });
+        }
+        resolve()
+      }).catch(error => {
+        console.log(error);
+        reject(error);
+      });
     });
   };
 }
